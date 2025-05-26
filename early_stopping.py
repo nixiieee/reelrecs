1057c3b0c65dc528bd31419bb3903aa4cf58bf3a
sergfedchin <sergfedchin@gmail.com>
2025-05-26 21:56:32 +0300

    First experiments

---
+++ early_stopping.py
@@ -0,0 +1,71 @@
+from typing import Callable, Literal
+import numpy as np
+import torch
+
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self,
+                 patience: int = 7,
+                 threshold = 0,
+                 threshold_mode: Literal['rel', 'abs'] = 'abs',
+                 verbose: bool = False,
+                 path: str = 'checkpoint.pt',
+                 trace_func: Callable = print
+                 ):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+            path (str): Path for the checkpoint to be saved to.
+                            Default: 'checkpoint.pt'
+            trace_func (function): trace print function.
+                            Default: print
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.threshold_mode = threshold_mode
+        self.counter = 0
+        self.best_val_loss = None
+        self.early_stop = False
+        self.val_loss_min = np.inf
+        self.threshold = threshold
+        self.path = path
+        self.trace_func = trace_func
+
+    def _significant_improvement(self, val_loss) -> bool:
+        if self.threshold_mode == 'abs':
+            return self.best_val_loss - val_loss > self.threshold
+        else:
+            return (self.best_val_loss - val_loss) / self.best_val_loss > self.threshold
+
+    def __call__(self, val_loss, model):
+        # Check if validation loss is nan
+        if np.isnan(val_loss):
+            self.trace_func("Validation loss is NaN. Ignoring this epoch.")
+            return
+
+        if self.best_val_loss is None:
+            self.best_val_loss = val_loss
+            self.save_checkpoint(val_loss, model)
+        elif self._significant_improvement(val_loss):
+            self.best_val_loss = val_loss
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0  # Reset counter since improvement occurred
+        else:
+            # No significant improvement
+            self.counter += 1
+            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+
+    def save_checkpoint(self, val_loss: float, model: torch.nn.Module):
+        '''Saves model when validation loss decreases.'''
+        if self.verbose:
+            self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
